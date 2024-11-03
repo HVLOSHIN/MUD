739a74139fc0da22393c162b6121332e77849055
HVLOSHIN <halogod9985@naver.com>
2024-11-03 14:39:10 +0900

    feat-Stats : UsrCombatDTO

---
+++ src/main/java/HVLO/TEXTRPG/user/dto/UserDTO.java
@@ -0,0 +1,26 @@
+package HVLO.TEXTRPG.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class UserDTO {
+    private Long userid;
+
+    private String username;
+
+    private UserStatsDTO userStats;
+
+    private UserAchievementsDTO achievements;
+
+    private List<UserLogDTO> logs;
+
+    private List<UserEquipmentDTO> equipments;
+
+    private List<UserMasteryDTO> mastery;
+
+   private UserCombatDTO combat;
+
+}
